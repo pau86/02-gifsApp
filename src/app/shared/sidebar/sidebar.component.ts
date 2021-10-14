690bbf41c67c7a3bf8549f42c7a49cf9b335d234
Paulina Pérez <paulinabp8601@gmail.com>
2021-10-14 17:16:32 +0200

    Video 81 - Create GiftService

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
 })
 
 export class SidebarComponent {
+
+  constructor( private gifsService: GifsService ) {}
+
+  get historial() {
+    return this.gifsService.historial;
+  }
+
 
 }
