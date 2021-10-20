a87867dcb1b879f599cedcc2b5e9509a16d019ad
Paulina Pérez <paulinabp8601@gmail.com>
2021-10-20 11:12:11 +0200

    Video 87 - LocalStorage | Save the user searches

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -16,7 +16,11 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http:HttpClient ) {}
+  constructor( private http:HttpClient ) {
+
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || []
+
+  }
 
   buscarGifs( query: string = ' ' ) {
 
@@ -25,6 +29,8 @@ export class GifsService {
     if( !this._historial.includes(query) ) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ));
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=jBQc36QHNZjJjIHkSQEVfXR2b4OzMWHF&q=${ query }&limit=10`)
